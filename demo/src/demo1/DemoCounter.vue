11fabcfa96b7b6e94178eafeb6e9559fe3d56b96
Roman Skobenkov <skobenkovroman@gmail.com>
2024-07-16 02:30:21 +0300

    step 2.9 - Взаимодействие внутри компонентов счётчика

---
+++ demo/src/demo1/DemoCounter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="counter">
+    <counter-button @action="decrement">-</counter-button>
+
+    <counter-value :value="count" />
+
+    <counter-button @action="increment">+</counter-button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import CounterButton from "./CounterButton.vue";
+import CounterValue from "./CounterValue.vue";
+
+const count = ref(0)
+
+const decrement = () => {
+  count.value = count.value - 1
+}
+
+const increment = () => {
+  count.value = count.value + 1
+}
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+}
+</style>
--- demo/src/routes.js
+++ demo/src/routes.js
@@ -1,5 +1,5 @@
 export default [{
   name: 'Counter Base',
   path: '/counter-0',
-  component: () => import("./demo0/DemoCounter.vue"),
+  component: () => import("./demo1/DemoCounter.vue"),
 },]
