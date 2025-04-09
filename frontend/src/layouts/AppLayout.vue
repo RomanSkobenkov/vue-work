ed092a0661f2f77fbb52e3f416ab59368d50bde8
Roman Skobenkov <r.skobenkov@intensa.ru>
2025-04-09 23:04:46 +0300

    part-1 1.38 Добавление стилей в шаблон в проекте Vue-Work

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -8,3 +8,11 @@ import AppLayoutHeader from "./AppLayoutHeader.vue";
     <slot />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.app_layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+</style>
