696e0d412643bc9d7d0d2dfc147e1ab5595b901f
Roman Skobenkov <r.skobenkov@intensa.ru>
2025-04-17 00:48:41 +0300

    part-2 2.9 Взаимодействие внутри компонентов счётчика Demo

---
+++ demo/src/demo1/DemoCounter.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import DemoContainer from "@/components/DemoContainer.vue";
+import CounterButton from "@/demo1/CounterButton.vue";
+import CounterValue from "@/demo1/CounterValue.vue";
+
+const count = ref(0)
+
+/* Метод уменьшения значения */
+const decrement = () => {
+  count.value = count.value - 1
+}
+
+/* Метод увеличения значения */
+const increment = () => {
+  count.value = count.value + 1
+}
+</script>
+
+<template>
+  <demo-container>
+    <div class="counter">
+      <counter-button @action="decrement">-</counter-button>
+
+      <counter-value :value="count" />
+
+      <counter-button @action="increment">+</counter-button>
+    </div>
+  </demo-container>
+</template>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+}
+</style>
